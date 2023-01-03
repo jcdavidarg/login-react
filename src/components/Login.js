4681d4bc64d5e8860b48d5d8152b92bcc8242340
jcdavidarg <jcdavidarg@gmail.com>
2023-01-03 03:37:01 -0300

    expresion regular

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,8 +7,17 @@ function Login() {
     const email = e.target.email.value;
     const password = e.target.password.value;
 
+    const regexEmail =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     if (email === "" || password === "") {
       console.log("Los campos no pueden estar vacios");
+    } else if (email !== "" && !regexEmail.test(email)) {
+      console.log("Debes escribir una dirección de correo electrónico válida");
+    } else if (email !== "challenge@alkemy.org" || password !== "react") {
+      console.log("Credenciales inválidas");
+    } else {
+      console.log("Estamos listos para enviar la información");
     }
   };
 
@@ -17,7 +26,7 @@ function Login() {
       <h2>FORMULARIO DE LOGIN</h2>
       <form onSubmit={submitHandler} className="formulario-contenedor">
         <label>EMAIL</label>
-        <input type="email" name="email" />
+        <input type="text" name="email" />
         <label>CONTRASEÑA</label>
         <input type="password" name="password" />
         <button type="submit">Ingresar</button>

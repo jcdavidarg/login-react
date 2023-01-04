89360ca0a63ff3971c41dcbad2d45d3d59d3fbe7
jcdavidarg <jcdavidarg@gmail.com>
2023-01-04 03:04:01 -0300

    componente Listado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./components/Login";
+import Listado from "./components/Listado";
 
 function App() {
   return (
     <div className="contenedor-app">
       <Login />
+      <Listado />
     </div>
   );
 }
---
+++ src/components/Listado.js
@@ -0,0 +1,5 @@
+function Listado() {
+  return <h2>Soy el componente Listado</h2>;
+}
+
+export default Listado;
